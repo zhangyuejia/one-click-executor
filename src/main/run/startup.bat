b64488b803eeff634be7d4275a3b571dd8cba62c
zhangyuejia <1098922863@qq.com>
2019-10-25 08:47:48 +0800

    '创建空分支3.0'

--- src/main/run/startup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem 去掉下面的rem可以指定运行的jdk
-rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_181
-rem set CLASSPATH=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOMe%\lib\tools.jar;
-rem set Path=%JAVA_HOME%\bin;
-java -jar make-copylist-2.0.3.jar
-pause
