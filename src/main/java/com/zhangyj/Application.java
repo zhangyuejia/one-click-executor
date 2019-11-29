ca3e8e8849124aabf7def0e751f854bf6dcda572
zhangyuejia <1098922863@qq.com>
2019-11-29 10:31:35 +0800

    copyListMakerV3.0.2正式打包

--- src/main/java/com/zhangyj/Application.java
+++ src/main/java/com/zhangyj/Application.java
@@ -5,13 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author ZHANG
  */
 @Slf4j
-@EnableAsync
 @SpringBootApplication
 public class Application {
 
