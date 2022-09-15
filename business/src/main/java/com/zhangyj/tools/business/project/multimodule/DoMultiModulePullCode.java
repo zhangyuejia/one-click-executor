8ad3bd263316301e61195ff833e236af1421ca61
zhangyuejia <1098922863@qq.com>
2022-09-15 16:26:05 +0800

    调整配置文件

--- business/src/main/java/com/zhangyj/tools/business/project/multimodule/DoMultiModulePullCode.java
+++ business/src/main/java/com/zhangyj/tools/business/project/multimodule/DoMultiModulePullCode.java
@@ -28,6 +28,11 @@ public class DoMultiModulePullCode extends AbstractRunner<MultiModulePullCodeCon
 
     private final static String LOCAL_BRANCH_FLAG = "  ";
 
+    private final static String BRANCH_SP = "/";
+
+    private final static String REMOTE_BRANCH_FLAG = LOCAL_BRANCH_FLAG + "remotes" + BRANCH_SP;
+
+
     @Override
     protected void doRun() throws IOException {
         // 是否检查只有一个replaceId
@@ -40,17 +45,68 @@ public class DoMultiModulePullCode extends AbstractRunner<MultiModulePullCodeCon
             }
             log.info("启用配置ID:{}", moduleProperties.getRefId());
             for (ModuleProperties.ModulesParam modulesParam : moduleProperties.getModulesParams()) {
-                String modulePath = moduleProperties.getProjectPath() + File.separator + modulesParam.getName();
+                String modulePath = getModulePath(moduleProperties, modulesParam);
                 if(!new File(modulePath).exists()){
                     log.error("模块{}路径不存在，跳过:{}", modulesParam.getName(), modulePath);
                     continue;
                 }
+                fetchRepo(moduleProperties, modulesParam);
                 // 切换到本地分支
                 checkoutLocalBranch(moduleProperties, modulesParam);
                 // 更新代码
                 pullCode(moduleProperties, modulesParam);
             }
         }
+    }
+
+    private void fetchRepo(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam) throws IOException {
+        String[] cmdArr = {"git", "fetch", "--all"};
+        logExecCmdOutput(moduleProperties, modulesParam, cmdArr);
+    }
+
+    private void pullCode(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam) throws IOException {
+        for (String remoteBranch : modulesParam.getRemoteBranch()) {
+            String[] cmdArr = {"git", "pull", moduleProperties.getRemoteRepoName(), remoteBranch};
+            logExecCmdOutput(moduleProperties, modulesParam, cmdArr);
+        }
+    }
+
+    private void checkoutLocalBranch(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam) throws IOException {
+        if (!moduleProperties.getEnableCheckoutLocalBranch()) {
+            log.info("未开启本地分支切换开关，无需切换");
+            return;
+        }
+        List<String> commandOutput = getCommandOutput(moduleProperties, modulesParam, new String[]{"git", "branch", "-a"});
+        String currentBranch = commandOutput.stream().filter(v -> v.startsWith(CURRENT_BRANCH_FLAG)).collect(Collectors.toList()).get(0).substring(CURRENT_BRANCH_FLAG.length());
+        String localBranch = modulesParam.getLocalBranch().trim();
+        if(currentBranch.equals(localBranch)){
+            log.info("{}仓库处于本地分支{}，无需切换", modulesParam.getName(), modulesParam.getLocalBranch());
+        } else if(commandOutput.contains(LOCAL_BRANCH_FLAG + localBranch)){
+            log.info("{}仓库处于其他分支{}，需要切换为{}", modulesParam.getName(), currentBranch, modulesParam.getLocalBranch());
+            String[] cmdArr = {"git", "checkout", localBranch};
+            logExecCmdOutput(moduleProperties, modulesParam, cmdArr);
+        }else if(commandOutput.contains(REMOTE_BRANCH_FLAG + moduleProperties.getRemoteRepoName() + BRANCH_SP + localBranch)){
+            log.info("{}仓库不存在分支，从远程仓库{}检出分支{}", modulesParam.getName(), moduleProperties.getRemoteRepoName(), modulesParam.getLocalBranch());
+            String[] cmdArr = {"git", "checkout", "-b", localBranch, moduleProperties.getRemoteRepoName() + BRANCH_SP + modulesParam.getLocalBranch()};
+            logExecCmdOutput(moduleProperties, modulesParam, cmdArr);
+        }else {
+            throw new RuntimeException("远程仓库不存在分支" + modulesParam.getLocalBranch() + ",无法进行检出");
+        }
+    }
+
+    private void logExecCmdOutput(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam, String[] cmdArr) throws IOException {
+        List<String> commandOutput = getCommandOutput(moduleProperties, modulesParam, cmdArr);
+        commandOutput.forEach(log::info);
+        checkOutput(commandOutput);
+    }
+
+    private List<String> getCommandOutput(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam, String[] cmdArr) throws IOException {
+        String modulePath = getModulePath(moduleProperties, modulesParam);
+        return CommandUtil.getCommandOutput(CharSets.CHARSET_GBK, cmdArr, modulePath);
+    }
+
+    private String getModulePath(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam){
+        return moduleProperties.getProjectPath() + File.separator + modulesParam.getName();
     }
 
     private void checkOutput(List<String> commandOutput) {
@@ -61,37 +117,5 @@ public class DoMultiModulePullCode extends AbstractRunner<MultiModulePullCodeCon
                 }
             }
         }
-    }
-
-    private void pullCode(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam) throws IOException {
-        String modulePath = moduleProperties.getProjectPath() + File.separator + modulesParam.getName();
-        for (String remoteBranch : modulesParam.getRemoteBranch()) {
-            logOutput(CommandUtil.getCommandOutput(CharSets.CHARSET_GBK, new String[]{"git", "pull", moduleProperties.getRemoteRepoName(), remoteBranch}, modulePath));
-        }
-    }
-
-    private void checkoutLocalBranch(ModuleProperties moduleProperties, ModuleProperties.ModulesParam modulesParam) throws IOException {
-        if (!moduleProperties.getEnableCheckoutLocalBranch()) {
-            log.info("未开启本地分支切换开关，无需切换");
-            return;
-        }
-        String modulePath = moduleProperties.getProjectPath() + File.separator + modulesParam.getName();
-        List<String> commandOutput = CommandUtil.getCommandOutput(CharSets.CHARSET_GBK, new String[]{"git", "branch"}, modulePath);
-        String currentBranch = commandOutput.stream().filter(v -> v.startsWith(CURRENT_BRANCH_FLAG)).collect(Collectors.toList()).get(0).substring(CURRENT_BRANCH_FLAG.length());
-        String localBranch = modulesParam.getLocalBranch().trim();
-        if(currentBranch.equals(localBranch)){
-            log.info("{}仓库处于本地分支{}，无需切换", modulesParam.getName(), modulesParam.getLocalBranch());
-        } else if(commandOutput.contains(LOCAL_BRANCH_FLAG + localBranch)){
-            log.info("{}仓库处于其他分支{}，需要切换为{}", modulesParam.getName(), localBranch, modulesParam.getLocalBranch());
-            logOutput(CommandUtil.getCommandOutput(CharSets.CHARSET_GBK, new String[]{"git", "checkout", localBranch}, modulePath));
-        }else {
-            log.info("{}仓库不存在分支，从远程仓库{}检出分支{}", modulesParam.getName(), moduleProperties.getRemoteRepoName(), modulesParam.getLocalBranch());
-            logOutput(CommandUtil.getCommandOutput(CharSets.CHARSET_GBK, new String[]{"git", "checkout", "-b", localBranch, moduleProperties.getRemoteRepoName() + "/" + modulesParam.getLocalBranch()}, modulePath));
-        }
-    }
-
-    private void logOutput(List<String> commandOutput) {
-        commandOutput.forEach(log::info);
-        checkOutput(commandOutput);
     }
 }
