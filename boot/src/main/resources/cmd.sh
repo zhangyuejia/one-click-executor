eb43fe2e427abbc5e79dd6306c11800d5bf5eb5c
zhangyuejia <1098922863@qq.com>
2022-09-22 19:48:05 +0800

    自动替换默认路径

--- boot/src/main/resources/cmd.sh
+++ boot/src/main/resources/cmd.sh
@@ -1,8 +1,8 @@
 ## 检出umc源码功能
-#component,exec
-#component,pull-code
-#shell,cmd /c mklink /j #{[dir]}\umc-portal\business\src\main\java\com\montnets\umc\portal\audit #{[dir]}\umc-portal-audit\src\main\java\com\montnets\umc\portal\audit
-#component,replace-properties
+component,exec
+component,pull-code
+shell,cmd /c mklink /j #{[dir]}\umc-portal\business\src\main\java\com\montnets\umc\portal\audit #{[dir]}\umc-portal-audit\src\main\java\com\montnets\umc\portal\audit
+component,replace-properties
 
 ## 替换配置
 #component,replace-properties
