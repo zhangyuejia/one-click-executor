e4da6226579c158326dc148284fad36cebfe5abc
zhangyuejia <1098922863@qq.com>
2022-09-27 09:47:50 +0800

    提交jar包打包代码

--- run/exec.sh
+++ run/exec.sh
@@ -1,27 +1,27 @@
 ## !!!变量说明：dir:指application.yml中dir resourceDir:指本项目resource绝对路径
 ##############################【功能】合并文件夹下的文件################################
-#component,splice-file,#{[classpath]}component\config\splice-file.yml
+#component,splice-file,.\run\conf\splice-file.yml
 
 ##############################【功能】检出源码并检出为指定分支功能########################
 ## clone代码
-#component,exec,#{[classpath]}component\config\exec.yml
+#component,exec,.\run\conf\exec.yml
 ## 更新代码
-#component,pull-code,#{[classpath]}component\config\pull-code.yml
+#component,pull-code,.\run\conf\pull-code.yml
 ## 挂载audit模块代码到business
 #shell,cmd /c mklink /j #{[dir]}\umc-portal\business\src\main\java\com\montnets\umc\portal\audit #{[dir]}\umc-portal-audit\src\main\java\com\montnets\umc\portal\audit,#{[dir]}
 ## 替换本地properties配置
-#component,replace-properties,#{[classpath]}component\config\replace-properties.yml
+#component,replace-properties,.\run\conf\replace-properties.yml
 
 ##############################【功能】替换本地properties配置##############################
-#component,replace-properties,#{[classpath]}component\config\replace-properties.yml
+#component,replace-properties,.\run\conf\replace-properties.yml
 
 ##############################【功能】更新后端代码########################################
-component,pull-code,#{[classpath]}component\config\pull-code.yml
+component,pull-code,.\run\conf\pull-code.yml
 
 ##############################【功能】更新前端代码########################################
-component,exec,#{[classpath]}component\config\exec-pull-web.yml
+component,exec,.\run\conf\\exec-pull-web.yml
 
 ##############################【功能】更新前端代码并启动###################################
-#component,exec,#{[classpath]}component\config\exec-start-web.yml
+#component,exec,.\run\conf\exec-start-web.yml
 
 
