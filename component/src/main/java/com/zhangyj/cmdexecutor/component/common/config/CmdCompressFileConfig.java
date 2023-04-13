44ac041ecc33133c75ec467f01bf0e46a52e241e
zhangyuejia <1098922863@qq.com>
2023-04-13 22:06:15 +0800

    添加压缩文件功能

---
+++ component/src/main/java/com/zhangyj/cmdexecutor/component/common/config/CmdCompressFileConfig.java
@@ -0,0 +1,32 @@
+package com.zhangyj.cmdexecutor.component.common.config;
+
+import com.zhangyj.cmdexecutor.core.common.config.AbstractCmdConfig;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 压缩文件
+ * @author zhagnyj
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Slf4j
+public class CmdCompressFileConfig extends AbstractCmdConfig {
+
+    /**
+     * 压缩目录
+     */
+    private String compressDir;
+
+    /**
+     * 压缩密码
+     */
+    private String compressPassword;
+
+
+    @Override
+    public String getDesc() {
+        return "压缩文件功能";
+    }
+}
